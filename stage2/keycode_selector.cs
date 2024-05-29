13e70cda0be3562323e230f7c78c634d0a29be79
jinborim <121596050+jinborim@users.noreply.github.com>
2024-05-30 01:09:33 +0900

    Create keycode_selector.cs

---
+++ stage2/keycode_selector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keycode_selector : MonoBehaviour
+{
+    public KeyCode[] keyCodes = {
+    KeyCode.Alpha1,
+    KeyCode.Alpha2,
+    KeyCode.Alpha3,
+    KeyCode.Alpha4,
+    KeyCode.Alpha5,
+    KeyCode.Alpha6,
+    KeyCode.Alpha7,
+    KeyCode.Alpha8,
+    KeyCode.Alpha9,
+};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        // Update is called once per frame
+
+    }
+}
