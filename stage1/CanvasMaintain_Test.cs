c58330693d005a828c172ed1e04d4bbf58b1cbf7
Jin Borim <121596050+jinborim@users.noreply.github.com>
2025-12-23 01:31:57 +0900

    Update CanvasMaintain_Test.cs

--- stage1/CanvasMaintain_Test.cs
+++ stage1/CanvasMaintain_Test.cs
@@ -5,18 +5,25 @@ using UnityEngine.UI;
 
 public class CanvasMaintain_Test : MonoBehaviour
 {
-    public static CanvasMaintain_Test Instance;
+/**
+* 씬이 바뀌어도 파괴되지 않고, 게임 내에서 단 하나만 존재해야하는 관리자 오브젝트를 만들 떼 사용
+**/
+    public static CanvasMaintain_Test Instance; // 다른 스크립트에서 CanvasMaintain_Test로 즉시 접근 할 수 있게 함
     
 
     //public Color TeamColor;
     private void Awake()
     {
+        // 이미 Instance에 등록되어 있다면
         if (Instance != null)
         {
+        // 새로 생성된 오브젝를 삭제
             Destroy(gameObject);
-            return;
+            return; //이후 코드 종료
         }
-        Instance = this;
+        Instance = this; //Instance가 비어있다면 자신을 위한 인스턴스로 등록
+        // 다음 스테이지로 넘어가더라도 이 오브젝트를 삭제하지 않음
+        // 캔버스의 데이터나 게임 전체에 유지
         DontDestroyOnLoad(gameObject);
         
     }
