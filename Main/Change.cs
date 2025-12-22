b8eab771712cd8178c43279019626c3447b913b1
Jin Borim <121596050+jinborim@users.noreply.github.com>
2025-12-23 00:27:08 +0900

    Update Change.cs

--- Main/Change.cs
+++ Main/Change.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Change : MonoBehaviour
 {
-//메인화면에서 다음 씬으로 넘어가기 위한 코드
-
+/**
+* 메인화면에서 다음 씬으로 넘어가기 위한 코드
+**/
     public void SceneChange()
     {
         SceneManager.LoadScene("intro"); //전환할 씬 이름
