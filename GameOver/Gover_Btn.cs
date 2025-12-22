d3b265f888a8d6eb63654135d72f57832b2c7934
Jin Borim <121596050+jinborim@users.noreply.github.com>
2025-12-23 01:40:06 +0900

    Update Gover_Btn.cs

--- GameOver/Gover_Btn.cs
+++ GameOver/Gover_Btn.cs
@@ -6,13 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class Gover_Btn : MonoBehaviour, IPointerClickHandler
 {
-    public STAGE_Maintain stage;
+    public STAGE_Maintain stage; //현재 정보를 가지고 있는 클래스 참조
 
+    //마우스로 오브젝트를 클릭했을 때 자동으로 실행되는 함수
     public void OnPointerClick(PointerEventData eventData)
     {
+        //클릭한 버튼이 마우스 왼족 버튼이면
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-            //SceneManager.LoadScene(stage.sceneNow);
+            //stage 스크립트에 저장된 현재 가야할 씬 이름으로 이동
             LoadSceneManager.LoadScene(stage.sceneNow);
         }
     }
@@ -20,6 +22,7 @@ public class Gover_Btn : MonoBehaviour, IPointerClickHandler
     // Start is called before the first frame update
     void Start()
     {
+        //STAGE_Maintain 타입의 오브젝트를 찾아 연걸
         stage = GameObject.FindObjectOfType<STAGE_Maintain>();
     }
 
