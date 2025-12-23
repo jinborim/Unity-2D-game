f14d3f94b35ac0e9d8217ec13f346e258f7a87b9
Jin Borim <121596050+jinborim@users.noreply.github.com>
2025-12-24 02:29:15 +0900

    Update Select_Yes.cs

--- stage1/Select_Yes.cs
+++ stage1/Select_Yes.cs
@@ -8,88 +8,85 @@ using UnityEngine.SceneManagement;
 
 public class Select_Yes : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    [Header("UI Components")]
+    private Color color_; // 버튼의 시각적 변화를 줄 Image 컴포넌트
+    public Image Panel_Img; 
 
-    private Color color_;
-    public Image Panel_Img;
-    public string sceneName;
-    public bool Panel_activated = false;
+    [Header("Settings")]
+    public string sceneName; // 로드하고자 하는 목적지 씬 이름
+    public bool Panel_activated = false; // 현재 버튼의 활성화(마우스 오버) 상태
 
     
-
+    //외부 시스템 에서 목적지 씬 이름을 전달받는 메서드
     public void GetSceneName(string _sceneName)
     {
         sceneName = _sceneName;
         //Debug.Log(sceneName);
     }
 
-
+    // 마우스 포인터가 UI 영역에 진입했을 때 호출
     public void OnPointerEnter(PointerEventData eventData)
     {
 
         Panel_activated = true;
-        Panel_ColorChanger(true);
+        Panel_ColorChanger(true); 
 
     }
 
+    // 마우스 포인터가 UI 영역을 벗어났을 때 호출
     public void OnPointerExit(PointerEventData eventData)
     {
 
-        Panel_activated = false;
+        Panel_activated = false; 
         Panel_ColorChanger(false);
 
     }
 
+    // UI 클릭시 발생하는 이벤트를 처리함
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Left)
+        // 마우스 왼쪽 버튼을 클릭 여부 및 활성화 상태 확인
+        if (eventData.button == PointerEventData.InputButton.Left && Panel_activated)
         {
-            if (Panel_activated == true)
-            {
-                //Debug.Log(sceneName);
-                //SceneManager.LoadScene(sceneName);
-                GameObject.Find("DialogBase").gameObject.SetActive(false);
-                GameObject.Find("SelectedBase").gameObject.SetActive(false);
+                // 현재 화면에 표시된 UI 비활성화
+                GameObject.Find("DialogBase").SetActive(false);
+                GameObject.Find("SelectedBase").SetActive(false);
+
+                // LoadSceneManger를 통한 비동기 또는 커스텀 씬 로딩 실행
                 LoadSceneManager.LoadScene(sceneName);
+
+                //데이터 초기화
                 sceneName = "";
+        }
 
-                
-            }
-            else
+
+        //상태값에 따라 버튼의 투명도를 조절함
+        public void Panel_ColorChanger(bool isActivated)
+        {
+            // 기본 색상은 유지하고 투명도만 변경하여 하이라이트 효과 부여
+            color_ = Color.white;
+            // 참이면 1.0f 거짓이면 0.8f
+            // color_a가 아니라 color_.a라고 해야함 변수로 인식할 수도 있음
+            color_.a = isActivated ? 1.0f : 0.8f;
+
+            // 색상을 UI 이미지에 반영
+            if(Panel_Img != null) // Pane_Img가 있는지 확인
             {
-
+                Panel_Img.color = color_;
             }
         }
-    }
 
-    public void Panel_ColorChanger(bool Panelactivated_)
-    {
-        if (Panel_activated == true)
+        void Start()
         {
-            color_ = new Color(1, 1, 1, 1);
-            color_.a = 1;
-            Panel_Img.color = color_;
+            // 컴포넌트 자동 할당을 통한 예외 방지
+            if(Panel_Img == null)
+            {
+                Panel_Img = GetComponent<Image>();
+            }
         }
-        else
+
+        void Update()
         {
-            color_ = new Color(1, 1, 1, 1);
-            color_.a = 0.8f;
-            Panel_Img.color = color_;
+        
         }
     }
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Panel_Img = transform.GetComponent<Image>();
-        //sceneName = "";
-        //Panel_Img.color = color_;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
